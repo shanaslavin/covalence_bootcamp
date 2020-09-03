09aab0543205a3a97c00896c711f7ea6e713cf68
Shana Slavin <shanaslavin@gmail.com>
2020-09-03 18:12:21 -0400

    initializing repository

---
+++ javascript_practice/app.js
@@ -0,0 +1,25 @@
+var name = "Shana Slavin";
+
+const us_states = 50;
+
+var add = 5 + 4;
+
+function sayHello(){
+  return "Hello World!"
+}
+
+sayHello();
+
+function checkAge(name, age){
+  if(age < 21){
+    return "Sorry " + name + ", you aren't old enough to view this page"
+  }
+}
+
+var favVeggies = ["brocoli", "spinach", "asparigus"];
+
+checkAge("Charles", 21);
+checkAge("Abby", 27);
+checkAge("James", 18);
+checkAge("John", 17);
+
